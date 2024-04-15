9992735ad59d935e0ce2d25b774f1a7c8a0d277c
Andreas Leonardo Soenarto <Andreasleonardo.al@gmail.com>
2024-04-15 15:13:17 +0700

    DISS-45 finish for migration code

---
+++ database/migrations/2024_04_15_063727_create_spk_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('spk', function (Blueprint $table) {
+            $table->id();
+            $table->integer("spk_number")->nullable();
+            $table->string("production_status")->nullable();
+            $table->date("post_date")->nullable();
+            $table->date("due_date")->nullable();
+            $table->string("item_code")->nullable();
+            $table->integer("plan_quantity")->nullable();
+            $table->integer("complete_quantity")->nullable();
+            $table->integer("outstanding_quantity")->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('spk');
+    }
+};
