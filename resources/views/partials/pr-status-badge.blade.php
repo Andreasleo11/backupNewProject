9e801b044a55d41c62e405d2a8b65655d49e9f17
raymondlay023 <raymondlay023@gmail.com>
2024-05-10 09:45:48 +0700

    pr status color changed

--- resources/views/partials/pr-status-badge.blade.php
+++ resources/views/partials/pr-status-badge.blade.php
@@ -1,18 +1,42 @@
+<style>
+    .head-bg-warning {
+        background-color: rgba(255, 193, 7, 0.2);
+        /* Adjust the alpha value (0.7) for opacity */
+        color: #000;
+        /* Set text color */
+    }
+
+    .purchaser-bg-warning {
+        background-color: rgba(255, 193, 7, 0.45);
+        color: #000;
+    }
+
+    .gm-bg-warning {
+        background-color: rgba(255, 193, 7, 0.7);
+        color: #000;
+    }
+
+    .verificator-bg-warning {
+        background-color: rgba(255, 193, 7, 0.7);
+        color: #000;
+    }
+</style>
+
 @if ($pr->status === 5)
     <span class="badge text-bg-danger px-3 py-2 fs-6">REJECTED</span>
     {{-- After the maker signed --}}
 @elseif($pr->status === 1)
-    <span class="badge text-bg-warning px-3 py-2 fs-6">WAITING FOR DEPT
+    <span class="badge head-bg-warning px-3 py-2 fs-6">WAITING FOR DEPT
         HEAD</span>
     {{-- After the dept head signed --}}
 @elseif($pr->status === 6)
-    <span class="badge text-bg-warning px-3 py-2 fs-6">WAITING FOR PURCHASER</span>
+    <span class="badge purchaser-bg-warning px-3 py-2 fs-6">WAITING FOR PURCHASER</span>
     {{-- After the purchaser signed --}}
 @elseif ($pr->status === 7)
-    <span class="badge text-bg-warning px-3 py-2 fs-6">WAITING FOR
+    <span class="badge gm-bg-warning px-3 py-2 fs-6">WAITING FOR
         GM</span>
 @elseif($pr->status === 2)
-    <span class="badge text-bg-warning px-3 py-2 fs-6">WAITING FOR
+    <span class="badge verificator-bg-warning px-3 py-2 fs-6">WAITING FOR
         VERIFICATOR</span>
 @elseif($pr->status === 3)
     <span class="badge text-bg-warning px-3 py-2 fs-6">WAITING FOR
