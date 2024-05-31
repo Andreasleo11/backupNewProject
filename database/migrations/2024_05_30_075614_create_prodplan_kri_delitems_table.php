daf9f7ec6ae54f442920db320382e5a08645ce8e
Andreas Leonardo Soenarto <Andreasleonardo.al@gmail.com>
2024-05-31 09:07:45 +0700

    DISS-124 Model & Migration

---
+++ database/migrations/2024_05_30_075614_create_prodplan_kri_delitems_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('prodplan_kri_delitems', function (Blueprint $table) {
+            $table->id();
+            $table->string("item_code")->nullable();
+            $table->integer("item_bom_level")->nullable();
+            $table->string("item_pair")->nullable();
+            $table->integer("pair_bom_level")->nullable();
+            $table->string("item_wip")->nullable();
+            $table->integer("bom_level")->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('prodplan_kri_delitems');
+    }
+};
