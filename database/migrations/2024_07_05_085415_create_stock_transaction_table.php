5ef008f47647b1335236cce4085f33e7ca289a65
Andreas Leonardo Soenarto <Andreasleonardo.al@gmail.com>
2024-07-05 16:50:50 +0700

    DISS-183 migrationfinish

--- database/migrations/2024_07_05_085415_create_stock_transaction_table.php
+++ database/migrations/2024_07_05_085415_create_stock_transaction_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('stock_transaction', function (Blueprint $table) {
             $table->id();
+            $table->string('barcode_string');
+            $table->integer('stock_id');
+            $table->integer('dept_id')->nullable();
+            $table->datetime('in_time');
+            $table->boolean('is_out')->default(false);
+            $table->boolean('is_return')->default(false);
+            $table->string('receiver')->nullable();
+            $table->string('remark')->nullable();
+            $table->datetime('out_time')->nullable();
             $table->timestamps();
         });
     }
