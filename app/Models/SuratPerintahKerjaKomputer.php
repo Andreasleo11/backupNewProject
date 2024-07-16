0adba90d517362cf9306504d7c7ac717490ff7dc
Andreas Leonardo Soenarto <Andreasleonardo.al@gmail.com>
2024-07-16 10:52:44 +0700

    DISS-199 migration, model & controller

---
+++ app/Models/SuratPerintahKerjaKomputer.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SuratPerintahKerjaKomputer extends Model
+{
+    use HasFactory;
+    protected $table = 'surat_perintah_kerja_komputer';
+
+    protected $fillable = [
+        'no_dokumen',
+        'pelapor',
+        'dept',
+        'tanggal_lapor',
+        'judul_laporan',
+        'keterangan_laporan',
+        'pic',
+        'keterangan_pic',
+        'status_laporan',
+        'tanggal_selesai',
+        'tanggal_estimasi',
+    ];
+
+    public function deptRelation()
+    {
+        return $this->belongsTo(Departement::class);
+    }
+    
+}
