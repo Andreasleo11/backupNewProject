d6131eb7ca2ce62eb8b332d9b0b566a96db47f4e
Andreas Leonardo Soenarto <Andreasleonardo.al@gmail.com>
2024-07-19 10:46:00 +0700

    DISS-205 - edit and design

---
+++ app/Models/DetailSoftware.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DetailSoftware extends Model
+{
+    use HasFactory;
+    protected $table = 'detail_softwares';
+    protected $fillable = [
+        'master_inventory_id',
+        'software_id',
+        'software_name',
+        'license',
+        'remark',
+    ];
+
+    public function masterInventory()
+    {
+        return $this->belongsTo(MasterInventory::class);
+    }
+
+    public function softwareType()
+    {
+        return $this->belongsTo(SoftwareTypeInventory::class, 'software_id');
+    }
+    
+}
---
+++ database/seeders/SoftwareTypeInventorySeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+use App\Models\SoftwareTypeInventory;
+
+
+class SoftwareTypeInventorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $softwareTypes = [
+           'OS',
+           'Microsoft Office',
+        ];
+
+        foreach ($softwareTypes as $type) {
+            SoftwareTypeInventory::create(['name' => $type]);
+        }
+        
+    }
+}
