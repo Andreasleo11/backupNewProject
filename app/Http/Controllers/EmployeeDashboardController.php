6043c297f33d74ede937f196bb4026edba70cb72
Raymond Lay <77760136+raymondlay023@users.noreply.github.com>
2025-03-04 14:25:45 +0700

    DISS-273 add employee count per department chart

--- app/Http/Controllers/EmployeeDashboardController.php
+++ app/Http/Controllers/EmployeeDashboardController.php
@@ -57,7 +57,7 @@ class EmployeeDashboardController extends Controller
             $activeEmployeesQuery->where('Gender', $selectedGender);
         }
 
-        $activeEmployees = $activeEmployeesQuery->pluck('NIK')->toArray(); // Get filtered NIKs
+        $activeEmployees = $activeEmployeesQuery->whereNull('end_date')->pluck('NIK')->toArray(); // Get filtered NIKs
 
         $employeeData = [
             'total' => count($activeEmployees), // Total employees after filters
@@ -83,8 +83,8 @@ class EmployeeDashboardController extends Controller
                 $startDate = Carbon::now()->setISODate($year, $weekNumber)->startOfWeek()->toDateString();
                 $endDate = Carbon::now()->setISODate($year, $weekNumber)->endOfWeek()->toDateString();
 
-                \Illuminate\Support\Facades\Log::info("startDate : $startDate");
-                \Illuminate\Support\Facades\Log::info("endDate: $endDate");
+                // \Illuminate\Support\Facades\Log::info("startDate : $startDate");
+                // \Illuminate\Support\Facades\Log::info("endDate: $endDate");
 
                 // Apply week range filter
                 $evaluationQuery->whereBetween('Month', [$startDate, $endDate]);
@@ -115,7 +115,7 @@ class EmployeeDashboardController extends Controller
             return response()->json([]);
         }
 
-        \Illuminate\Support\Facades\Log::info($request->all());
+        // \Illuminate\Support\Facades\Log::info($request->all());
 
         $query = EvaluationDataWeekly::join('employees', 'evaluation_data_weekly.NIK', '=', 'employees.NIK')
                     ->join('departments', 'employees.Dept', '=', 'departments.dept_no')
@@ -175,9 +175,9 @@ class EmployeeDashboardController extends Controller
         $department = $request->department;
         $status = $request->status; // Status can be null if user clicks "Total Employees"
 
-        $query = Employee::join('departments', 'employees.Dept', '=', 'departments.dept_no')
+        $query = Employee::whereNull('end_date')->join('departments', 'employees.Dept', '=', 'departments.dept_no')
             ->where('departments.name', $department)
-            ->select('employees.NIK', 'employees.Nama', 'employees.employee_status');
+            ->select('employees.NIK', 'employees.Nama', 'employees.Branch', 'employees.employee_status');
 
         // Apply status filter only if a specific status is selected
         if ($status) {
@@ -199,7 +199,7 @@ class EmployeeDashboardController extends Controller
         $legend = $request->legend;
 
         // Base query
-        $query = Employee::join('departments', 'employees.Dept', '=', 'departments.dept_no')
+        $query = Employee::whereNull('end_date')->join('departments', 'employees.Dept', '=', 'departments.dept_no')
             ->select('employees.NIK', 'employees.Nama', 'departments.name as department_name', 'employees.employee_status');
 
         // Apply filters
@@ -238,7 +238,7 @@ class EmployeeDashboardController extends Controller
     private function getEmployeeCountByDepartment()
     {
         // Get all unique statuses across employees
-        $statuses = Employee::distinct()->pluck('employee_status')->toArray();
+        $statuses = Employee::whereNull('end_date')->distinct()->pluck('employee_status')->toArray();
 
         // Fetch employee count grouped by department and status
         $departments = Employee::join('departments', 'employees.Dept', '=', 'departments.dept_no')
@@ -284,7 +284,7 @@ class EmployeeDashboardController extends Controller
         $year = $year ?? date('Y'); // Default to current year if not provided
 
         try {
-            $employeeCounts = Employee::join('evaluation_datas', 'employees.NIK', '=', 'evaluation_datas.NIK')
+            $employeeCounts = Employee::whereNull('end_date')->join('evaluation_datas', 'employees.NIK', '=', 'evaluation_datas.NIK')
                 ->whereYear('evaluation_datas.Month', $year)
                 ->select(
                     DB::raw('MONTH(evaluation_datas.Month) as month'),
