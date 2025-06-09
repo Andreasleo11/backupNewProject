5a82eb81145f9547171eefb3e598a669c8c90aa2
Raymond Lay <77760136+raymondlay023@users.noreply.github.com>
2025-06-09 11:56:17 +0700

    overtime form change status to string value

--- resources/views/partials/formovertime-status.blade.php
+++ resources/views/partials/formovertime-status.blade.php
@@ -9,28 +9,5 @@
         @endif
     </div>
 @else
-    @switch($fot->status)
-        @case(1)
-            <span class="badge px-3 py-2 fs-6 bg-warning text-dark">Waiting for Dept Head</span>
-        @break
-
-        @case(2)
-            <span class="badge px-3 py-2 fs-6 bg-warning text-dark">Waiting for Verificator</span>
-        @break
-
-        @case(3)
-            <span class="badge px-3 py-2 fs-6 bg-warning text-dark">Waiting for GM</span>
-        @break
-
-        @case(9)
-            <span class="badge px-3 py-2 fs-6 bg-warning text-dark">Waiting Director</span>
-        @break
-
-        @case(6)
-            <span class="badge px-3 py-2 fs-6 bg-info text-dark">Waiting for Supervisor</span>
-        @break
-
-        @default
-            <span class="badge px-3 py-2 fs-6 bg-secondary">Unknown</span>
-    @endswitch
+    <x-overtime-form-status-badge :status="$fot->status" />
 @endif
