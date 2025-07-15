ac6cea63bcc4c80d154aabf7f152904e4f1bc302
Raymond Lay <77760136+raymondlay023@users.noreply.github.com>
2025-07-15 11:44:10 +0700

    Delsched queue feature, progress still on dev

---
+++ app/Jobs/ProcessDeliveryScheduleStep4.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class ProcessDeliveryScheduleStep4 extends BaseDeliveryScheduleStep implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    public int $timeout = 300;
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    protected function executeStep(): void
+    {
+        app(\App\Services\DeliveryScheduleService::class)->runStep4();
+    }
+}
