60530999f6ccaddaa50763e48b34edcef09249be
Raymond Lay <77760136+raymondlay023@users.noreply.github.com>
2025-09-18 15:19:35 +0700

    DISS-295 small patch

--- app/View/Components/InspectionSection.php
+++ app/View/Components/InspectionSection.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class inspection-section extends Component
+class InspectionSection extends Component
 {
     /**
      * Create a new component instance.
@@ -21,6 +21,6 @@ class inspection-section extends Component
      */
     public function render(): View|Closure|string
     {
-        return view('components.inspection-section');
+        return view("components.inspection-section");
     }
 }
