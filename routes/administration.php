4c4ddc246930babc79313d26199f635b29240d91
Raymond Lay <77760136+raymondlay023@users.noreply.github.com>
2026-02-25 09:49:12 +0700

    fix: remove unused duplicate rule template feature

--- routes/administration.php
+++ routes/administration.php
@@ -3,8 +3,6 @@
 use App\Http\Controllers\ApprovalSignatureController;
 use App\Http\Controllers\FileController;
 use App\Http\Controllers\SignatureController;
-use App\Livewire\Admin\Approvals\RuleTemplates\Edit as RuleTemplatesEdit;
-use App\Livewire\Admin\Approvals\RuleTemplates\Index as RuleTemplatesIndex;
 use App\Livewire\Signature\CaptureSignature;
 use App\Livewire\Signature\ManageSignatures;
 use Illuminate\Support\Facades\Route;
@@ -27,16 +25,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth')->group(function () {
-    // Approval Rules & Templates
-    Route::middleware(['can:manage-approvals'])
-        ->prefix('admin/approvals')
-        ->name('admin.approvals.')
-        ->group(function () {
-            Route::get('/rules', RuleTemplatesIndex::class)->name('rules.index');
-            Route::get('/rules/create', RuleTemplatesEdit::class)->name('rules.create');
-            Route::get('/rules/{templateId}/edit', RuleTemplatesEdit::class)->name('rules.edit');
-        });
-
     // Signature Management
     Route::get('signatures/{id}', [SignatureController::class, 'show'])->name('signatures.show');
     Route::get('settings/signatures', ManageSignatures::class)->name('signatures.manage');
